eadb2bd32979afc2044ff9bba10bcd9628d9b56b
Akiranya <porridgen@gmail.com>
2023-01-11 01:56:44 +0800

    Fix: severe NPE if chatting in a town/nation which contains NPC residents

--- plugin/src/main/java/at/helpch/chatchat/hooks/towny/AbstractTownyChannel.java
+++ plugin/src/main/java/at/helpch/chatchat/hooks/towny/AbstractTownyChannel.java
@@ -5,6 +5,7 @@ import at.helpch.chatchat.api.holder.FormatsHolder;
 import at.helpch.chatchat.api.user.ChatUser;
 import at.helpch.chatchat.api.user.User;
 import at.helpch.chatchat.channel.AbstractChannel;
+import at.helpch.chatchat.user.UsersHolderImpl;
 import at.helpch.chatchat.util.ChannelUtils;
 import com.palmergames.bukkit.towny.TownyUniverse;
 import com.palmergames.bukkit.towny.object.Resident;
@@ -13,13 +14,14 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 public abstract class AbstractTownyChannel extends AbstractChannel {
+
     protected AbstractTownyChannel(@NotNull final String name,
                                    @NotNull final String messagePrefix,
                                    @NotNull final List<String> toggleCommands,
@@ -29,7 +31,8 @@ public abstract class AbstractTownyChannel extends AbstractChannel {
         super(name, messagePrefix, toggleCommands, channelPrefix, formats, radius);
         if (Bukkit.getPluginManager().getPlugin("Towny") == null) {
             throw new RuntimeException("Attempting to use a Towny channel but Towny is not installed.");
-        }}
+        }
+    }
 
     private Optional<ResidentList> residentList(@NotNull final UUID uuid) {
         return TownyUniverse.getInstance().getResidentOpt(uuid).map(this::residentList);
@@ -46,12 +49,26 @@ public abstract class AbstractTownyChannel extends AbstractChannel {
 
     @Override
     public Set<User> targets(final @NotNull User source) {
-        final var list = residentList(source.uuid());
-        if (list.isEmpty()) return Set.of();
-        return list.get().getResidents().stream()
-                .map(Resident::getUUID)
-                .map(plugin.usersHolder()::getUser)
-                .filter(target -> ChannelUtils.isTargetWithinRadius(source, target, radius()))
-                .collect(Collectors.toSet());
+        Optional<ResidentList> residentList = residentList(source.uuid());
+        if (residentList.isEmpty()) return Set.of();
+
+        // Start to construct the target list
+        Set<User> set = new HashSet<>();
+        UsersHolderImpl users = plugin.usersHolder();
+        for (Resident resident : residentList.get().getResidents()) {
+
+            // Caution: towns/nations may contain NPC residents whose UUID does not map to a real player.
+            // This leads to ChatChat creating broken User instances when calling UserHolder#getUser(UUID).
+            if (resident.isNPC()) continue;
+
+            UUID uuid = resident.getUUID();
+            User target = users.getUser(uuid);
+            if (ChannelUtils.isTargetWithinRadius(source, target, radius())) {
+                set.add(target);
+            }
+        }
+
+        return set;
     }
+
 }
